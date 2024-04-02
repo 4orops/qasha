e7ec744efde2a5d0bb86fff80fd339dc80fc9696
4orops <wabile4@gmail.com>
2024-04-02 11:13:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from './Components/Nav';
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <Nav />
+        <h1>QASHA</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,9 @@
+import qasha from '../assets/logo-qasha.png'
+
+export default function Nav() {
+    return(
+        <nav>
+            <img className='nav--logo' src= {qasha} alt='nav image'/>
+        </nav>
+    )
+}
